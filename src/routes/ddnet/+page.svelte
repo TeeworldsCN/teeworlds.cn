5aa77b99eed90dcd18ecfa101644b7a0e901da38
TsFreddie <whatis@tsdo.in>
2024-12-24 18:02:58 +0800

    add wiki external link

--- src/routes/ddnet/+page.svelte
+++ src/routes/ddnet/+page.svelte
@@ -14,3 +14,8 @@
 	<ToolboxButton href="/ddnet/maps/">查询地图</ToolboxButton>
 	<div class="text-semibold text-2xl">查找 DDNet 官方地图</div>
 </div>
+
+<div class="mt-8">
+	<ToolboxButton href="/link/?ref={encodeURIComponent("https://wiki.ddnet.org/wiki/Main_Page/zh")}">中文 Wiki</ToolboxButton>
+	<div class="text-semibold text-2xl">详细教程与操作指南</div>
+</div>
