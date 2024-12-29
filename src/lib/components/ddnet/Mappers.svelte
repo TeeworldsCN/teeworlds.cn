4ead05d022ac418182e2671c4b96194cb029ccd5
TsFreddie <whatis@tsdo.in>
2024-12-29 13:41:53 +0800

    fix ssr for mappers component

--- src/lib/components/ddnet/Mappers.svelte
+++ src/lib/components/ddnet/Mappers.svelte
@@ -3,17 +3,15 @@
 
 	const { authors, click }: { authors: string; click?: (author: string) => void } = $props();
 
-	let authorList: string[] = $state([]);
-
-	$effect(() => {
-		authorList = authors
+	let authorList = $derived(() =>
+		authors
 			.split(',')
 			.flatMap((author) => author.split('&'))
-			.map((author) => author.trim());
-	});
+			.map((author) => author.trim())
+	);
 </script>
 
-{#each authorList as author, i}
+{#each authorList() as author, i}
 	{#if click}
 		<Link
 			href="/ddnet/maps/#mapper=%22{encodeURIComponent(author)}%22"
