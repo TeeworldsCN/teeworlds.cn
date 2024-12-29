fbb18b215692cc850f6d2d415da0793c00351bd4
TsFreddie <whatis@tsdo.in>
2024-12-29 12:09:25 +0800

    prepare a basic framework for scheduled tasks

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,8 +1,23 @@
 import { persistent, volatile } from '$lib/server/keyv';
+import { mapReleases } from '$lib/server/tasks/map-releases';
 import type { ServerInit } from '@sveltejs/kit';
+import type { Cron } from 'croner';
+
+const initTasks = (...tasks: Cron[]) => {
+	const { __croner_tasks }: { __croner_tasks: Cron[] } = globalThis as any;
+	for (const task of __croner_tasks || []) {
+		task.stop();
+	}
+	(globalThis as any).__croner_tasks = tasks;
+};
 
 export const init: ServerInit = async () => {
 	// initialize the key-value store on launch
 	volatile;
 	persistent;
+
+	initTasks(
+		// initialize scheduled tasks
+		mapReleases
+	);
 };
