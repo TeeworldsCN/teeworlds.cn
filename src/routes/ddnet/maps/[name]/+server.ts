87d380452d26b8a5909132068dab12f7c5999d5e
TsFreddie <whatis@tsdo.in>
2025-02-14 20:32:26 +0800

    fix map head request

--- src/routes/ddnet/maps/[name]/+server.ts
+++ src/routes/ddnet/maps/[name]/+server.ts
@@ -2,14 +2,9 @@ import { maps } from '$lib/server/fetches/maps';
 import type { RequestHandler } from './$types';
 
 // Special HEAD request check for whether a map exists
-export const HEAD: RequestHandler = async ({ url }) => {
-	const name = url.searchParams.get('n');
-	if (!name) {
-		return new Response(null, { status: 404 });
-	}
-
+export const HEAD: RequestHandler = async ({ params }) => {
 	const mapList = await maps.fetch();
-	const found = mapList.findIndex((map) => map.name == name);
+	const found = mapList.findIndex((map) => map.name == params.name);
 	if (found < 0) {
 		return new Response(null, { status: 404 });
 	} else {
