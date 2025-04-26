2806ba8bfccda8ea5368047a8c7a2bef6a6bfd8d
TsFreddie <whatis@tsdo.in>
2025-04-26 20:47:54 +0800

    vite watch speed up

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,26 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import anymatch from 'anymatch';
+
+const matcher = anymatch([
+	`${__dirname}/**`,
+	`${__dirname}/.*/**`,
+	`!${__dirname}`,
+	`!${__dirname}/*`,
+	`!${__dirname}/src`,
+	`!${__dirname}/src/**`,
+	`!${__dirname}/static`,
+	`!${__dirname}/static/**`,
+	`!${__dirname}/.svelte-kit`,
+	`!${__dirname}/.svelte-kit/**`
+]);
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	server: {
+		watch: {
+			// vite has problem dealing with watched files, so we manually select files to watch
+			ignored: matcher
+		}
+	}
 });
