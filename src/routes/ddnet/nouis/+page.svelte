ffbd4f6399b33ad67f04d18a0028daa4b5a71807
TsFreddie <whatis@tsdo.in>
2025-11-10 21:33:34 +0800

    update nouis to be more local

--- src/routes/ddnet/nouis/+page.svelte
+++ src/routes/ddnet/nouis/+page.svelte
@@ -1,5 +1,5 @@
 <iframe
-	src="https://nou.is"
+	src="https://dieroll.cn/pages/nouis/"
 	class="h-[calc(100svh-5.8rem)] w-full rounded-xl border border-slate-600"
 	loading="lazy"
 	title="BiggerNouis"
